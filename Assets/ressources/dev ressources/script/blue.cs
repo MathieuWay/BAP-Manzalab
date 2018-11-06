8ecf883f9b71f08218ef3cf76a1d70546b5e2653
ravagium <alexandre.roblin@devinci.fr>
2018-11-06 14:27:58 +0100

    modif alex

---
+++ Assets/ressources/dev ressources/script/blue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blue : MonoBehaviour {
+    Rigidbody2D RawImage;
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void _Blue()
+    {
+        Color newColor = new Color(0.3f, 0.4f, 0.6f, 0.3f);
+    }
+}
