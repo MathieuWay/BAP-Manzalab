9479dcb674a9df0f3000ceb58ec1e152846e7dcb
ravagium <alexandre.roblin@devinci.fr>
2019-01-31 00:38:57 +0100

    création d'un menu pour avoir les trois application en une

---
+++ Assets/Projet Box 3D/Script/appeler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appeler : MonoBehaviour {
+    public void onClick()
+    {
+        Application.OpenURL("tel:" + 3900);
+    }
+}
---
+++ Assets/Scenes/changescene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour {
+
+    public void NextScene1()
+    {
+        SceneManager.LoadScene("Manzalab_card");
+    }
+    public void NextScene2()
+    {
+        SceneManager.LoadScene("Aug_Video");
+    }
+    public void NextScene3()
+    {
+        SceneManager.LoadScene("projet assistance");
+    }
+}
